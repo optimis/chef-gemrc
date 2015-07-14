527700838af95c07e39eec29891aca3bf4060340
John Bellone <jbellone@bloomberg.net>
2015-07-14 13:51:22 -0400

    Fixes a bug with loading the new gemfile into Chef.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,6 @@ gemrc_file '/root/.gemrc' do
 end
 
 ruby_block 'reload-gem-configuration' do
-  block { Gem.configuration = Gem::ConfigFile.new ['/root/.gemrc'] }
+  block { Gem.configuration = Gem::ConfigFile.new ['--config-file=/root/.gemrc'] }
   only_if { node['gemrc']['reload'] }
 end

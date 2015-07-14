fd9645f3725376ae4d4a026563bda7bfe55f248b
John Bellone <jbellone@bloomberg.net>
2015-07-14 13:54:59 -0400

    Executes resources in default recipe at compile time.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,8 @@
 gemrc_file '/root/.gemrc' do
   options node['gemrc']['config']
-end
+end.run_action(:create)
 
 ruby_block 'reload-gem-configuration' do
   block { Gem.configuration = Gem::ConfigFile.new ['--config-file=/root/.gemrc'] }
   only_if { node['gemrc']['reload'] }
-end
+end.run_action(:run)

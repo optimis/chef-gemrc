d224794fac8d1ce77b7ab1caab88d954d7ff595b
John Bellone <jbellone@bloomberg.net>
2015-07-28 11:09:18 -0400

    Fixes issue converging the resource on AIX hosts.
    By default it seems that AIX doesn't use root groupname. I guess this
    makes sense since old unicies use wheel. At any rate fixed.

--- libraries/gemrc_file.rb
+++ libraries/gemrc_file.rb
@@ -7,8 +7,8 @@ module GemrcCookbook
     provides(:gemrc_file)
 
     attribute(:path, kind_of: String, name_attribute: true)
-    attribute(:owner, kind_of: String, default: 'root')
-    attribute(:group, kind_of: String, default: 'root')
+    attribute(:owner, kind_of: [NilClass, String], default: nil)
+    attribute(:group, kind_of: [NilClass, String], default: nil)
     attribute(:mode, kind_of: String, default: '0644')
     attribute(:options, option_collector: true, default: { gem: '--no-ri --no-rdoc' })
 

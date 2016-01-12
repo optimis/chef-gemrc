ae66fe45e52092e43aaaf2db302cbbe3cef8320f
srhanson <srhans1@gmail.com>
2016-01-12 16:36:13 -0500

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'ops+cookbooks@optimiscorp.com'
 license 'Apache 2.0'
 description 'Library cookbook to configure Rubygems.'
 long_description 'Library cookbook to configure Rubygems.'
-version '1.0.1'
+version '1.0.2'
 
 depends 'poise', '~> 2.2.0'

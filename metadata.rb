d60e07c0e81bc0e39f1ef5fb55c6d4c610b2a685
srhanson <srhans1@gmail.com>
2016-01-12 16:35:49 -0500

    Update metadata.rb
    Use a newer version of poise

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description 'Library cookbook to configure Rubygems.'
 long_description 'Library cookbook to configure Rubygems.'
 version '1.0.1'
 
-depends 'poise', '~> 2.0'
+depends 'poise', '~> 2.2.0'

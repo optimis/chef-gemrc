11457876508ed70c0461f0a6c726e2d47b3ce97c
Shahul Khajamohideen <skhajamohid1@bloomberg.net>
2016-02-17 18:02:04 -0500

    Reorder statements to fix missing resource error

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,15 +4,14 @@
 #
 # Copyright 2015-2016, Bloomberg Finance L.P.
 #
-
-gemrc_file node['gemrc']['path'] do
-  options node['gemrc']['config']
-  notifies :run, 'ruby_block[reload-gem-configuration]', :delayed
-end.run_action(:create)
-
 arguments = ['--config-file', node['gemrc']['path']].join('=')
 ruby_block 'reload-gem-configuration' do
   action :nothing
   block { Gem.configuration = Gem::ConfigFile.new [arguments] }
   only_if { node['gemrc']['reload'] }
 end
+
+gemrc_file node['gemrc']['path'] do
+  options node['gemrc']['config']
+  notifies :run, 'ruby_block[reload-gem-configuration]', :delayed
+end.run_action(:create)
